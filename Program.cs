1868fabba92b1b6db54eefc399946dee9f767f28
miriamhwork <miriamhwork@hotmail.com>
2024-11-08 13:48:17 -0300

    Desafio: implementação das classes abstratas e específicas para dois tipos de celulares diferentes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "123456789", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Modelo 2", imei: "987654321", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
